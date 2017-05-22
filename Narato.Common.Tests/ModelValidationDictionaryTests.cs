f2fa99e3dbf18c2bc17cc2f11be33309420aa87b
Ronald Verhaegen <ronald@Ronalds-MacBook-Pro-2.local>
2017-05-22 14:42:27 +0200

    nuspec and tests

---
+++ Narato.Common.Tests/ModelValidationDictionaryTests.cs
@@ -0,0 +1,21 @@
+﻿using Narato.Common.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Narato.Common.Tests
+{
+    public class ModelValidationDictionaryTests
+    {
+        [Fact]
+        public void SerializeJson_ReturnCorrectJson()
+        {
+            var dictionary = new ModelValidationDictionary<string>();
+            dictionary.Add("FirstName", "John");
+            dictionary.Add("LastName", "Doe");
+
+            var json = JsonConvert.SerializeObject(dictionary);
+
+            Assert.Equal(json, "{\"FirstName\":[\"John\"],\"LastName\":[\"Doe\"]}");
+        }
+    }
+}
